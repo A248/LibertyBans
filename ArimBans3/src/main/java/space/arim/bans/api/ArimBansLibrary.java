9820705c2dbf3bf6f6189cd6dcfed103ccf44f24
A248 <anandebeh@gmail.com>
2019-11-24 08:02:37 -0500

    Move static field to top of class

--- ArimBans3/src/main/java/space/arim/bans/api/ArimBansLibrary.java
+++ ArimBans3/src/main/java/space/arim/bans/api/ArimBansLibrary.java
@@ -28,6 +28,30 @@ import space.arim.bans.api.util.Tools;
 public interface ArimBansLibrary extends PunishmentPlugin, AutoCloseable {
 
 	/**
+	 * Used internally for invalid messages. If you send a message whose sole content
+	 * is this, you will receive an unchecked exception.
+	 */
+	static String INVALID_STRING_CODE = "warning-invalid-string";
+	
+	/**
+	 * Checks a message for validity.
+	 * <br><br>All this does is:
+	 * <br>1. Check against {@link #INVALID_MESSAGE_CODE} for equality
+	 * <br>2. Check for null values
+	 * 
+	 * <br><Br>Throws an unchecked exception if either requirements are met.
+	 * 
+	 * @param message - the string to check
+	 */
+	static void checkString(String message) {
+		if (message == null) {
+			throw new InternalStateException("checkString: String is null!");
+		} else if (INVALID_STRING_CODE.equals(message)) {
+			throw new InternalStateException("checkString: String is INVALID_STRING_CODE!");
+		}
+	}
+	
+	/**
 	 * Gets a Subject from a UUID
 	 * 
 	 * @param uuid - the uuid to turn into a subject
@@ -153,30 +177,6 @@ public interface ArimBansLibrary extends PunishmentPlugin, AutoCloseable {
 	 */
 	default boolean checkAddress(String address) {
 		return Tools.validAddress(address);
-	}
-	
-	/**
-	 * Used internally for invalid messages. If you send a message whose sole content
-	 * is this, you will receive an unchecked exception.
-	 */
-	static String INVALID_STRING_CODE = "warning-invalid-string";
-	
-	/**
-	 * Checks a message for validity.
-	 * <br><br>All this does is:
-	 * <br>1. Check against {@link #INVALID_MESSAGE_CODE} for equality
-	 * <br>2. Check for null values
-	 * 
-	 * <br><Br>Throws an unchecked exception if either requirements are met.
-	 * 
-	 * @param message - the string to check
-	 */
-	static void checkString(String message) {
-		if (message == null) {
-			throw new InternalStateException("checkString: String is null!");
-		} else if (INVALID_STRING_CODE.equals(message)) {
-			throw new InternalStateException("checkString: String is INVALID_STRING_CODE!");
-		}
 	}
 	
 }
