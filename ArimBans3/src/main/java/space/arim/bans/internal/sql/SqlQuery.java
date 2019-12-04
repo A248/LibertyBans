0c410fcf4e778bec25a0e9631fefde7c373f3d60
A248 <anandebeh@gmail.com>
2019-12-04 17:24:14 -0500

    Simplify SqlQuery.Query
    Reduces changes of storage mode-specific bugs.

--- ArimBans3/src/main/java/space/arim/bans/internal/sql/SqlQuery.java
+++ ArimBans3/src/main/java/space/arim/bans/internal/sql/SqlQuery.java
@@ -47,16 +47,7 @@ public class SqlQuery {
 						+ "`operator` VARCHAR(" + SUBJECT_COLUMN_SIZE + ") NOT NULL,"
 						+ "`reason` VARCHAR(255) NOT NULL,"
 						+ "`expiration` BIGINT NOT NULL,"
-						+ "`date` BIGINT NOT NULL)",
-
-				"CREATE TABLE IF NOT EXISTS %PREFIX%active ("
-						+ "id INTEGER,"
-						+ "type VARCHAR(3),"
-						+ "subject VARCHAR(" + SUBJECT_COLUMN_SIZE + "),"
-						+ "operator VARCHAR(" + SUBJECT_COLUMN_SIZE + "),"
-						+ "reason VARCHAR(255),"
-						+ "expiration BIGINT,"
-						+ "date BIGINT)"),
+						+ "`date` BIGINT NOT NULL)"),
 
 		CREATE_TABLE_HISTORY(
 				"CREATE TABLE IF NOT EXISTS `%PREFIX%history` ("
@@ -66,16 +57,7 @@ public class SqlQuery {
 						+ "`operator` VARCHAR(" + SUBJECT_COLUMN_SIZE + ") NOT NULL,"
 						+ "`reason` VARCHAR(255) NOT NULL,"
 						+ "`expiration` BIGINT NOT NULL,"
-						+ "`date` BIGINT NOT NULL)",
-
-				"CREATE TABLE IF NOT EXISTS %PREFIX%history ("
-						+ "id INTEGER,"
-						+ "type VARCHAR(3),"
-						+ "subject VARCHAR(" + SUBJECT_COLUMN_SIZE + "),"
-						+ "operator VARCHAR(" + SUBJECT_COLUMN_SIZE + "),"
-						+ "reason VARCHAR(255),"
-						+ "expiration BIGINT,"
-						+ "date BIGINT)"),
+						+ "`date` BIGINT NOT NULL)"),
 
 		CREATE_TABLE_CACHE(
 				"CREATE TABLE IF NOT EXISTS `%PREFIX%cache` ("
@@ -83,89 +65,53 @@ public class SqlQuery {
 						+ "`name` VARCHAR(15) NOT NULL,"
 						+ "`iplist` TEXT NOT NULL,"
 						+ "`update_name` BIGINT NOT NULL," 
-						+ "`update_iplist` BIGINT NOT NULL)",
-				"CREATE TABLE IF NOT EXISTS %PREFIX%cache ("
-						+ "uuid VARCHAR(31),"
-						+ "name VARCHAR(15),"
-						+ "iplist TEXT,"
-						+ "update_name BIGINT,"
-						+ "update_iplist BIGINT)"),
+						+ "`update_iplist` BIGINT NOT NULL)"),
 
 		INSERT_ACTIVE(
 				"INSERT INTO `%PREFIX%active` "
 						+ "(`id`, `type`, `subject`, `operator`, `reason`, `expiration`, `date`) "
-						+ "VALUES (?, ?, ?, ?, ?, ?, ?)",
-
-				"INSERT INTO %PREFIX%active "
-						+ "(id, type, subject, operator, reason, expiration, date) "
 						+ "VALUES (?, ?, ?, ?, ?, ?, ?)"),
 
 		INSERT_HISTORY(
 				"INSERT INTO `%PREFIX%history` "
 						+ "(`id`, `type`, `subject`, `operator`, `reason`, `expiration`, `date`) "
-						+ "VALUES (?, ?, ?, ?, ?, ?, ?)",
-
-				"INSERT INTO %PREFIX%history "
-						+ "(id, type, subject, operator, reason, expiration, date) "
 						+ "VALUES (?, ?, ?, ?, ?, ?, ?)"),
 
 		INSERT_CACHE("INSERT INTO `%PREFIX%cache` " 
 				+ "(`uuid`, `name`, `iplist`, `update_name`, `update_iplist`) " 
-				+ "VALUES (?, ?, ?, ?, ?)",
-
-				"INSERT INTO %PREFIX%cache " 
-				+ "(uuid, name, iplist, update_name, update_iplist) " 
 				+ "VALUES (?, ?, ?, ?, ?)"),
 
 		DELETE_ACTIVE_BY_ID(
-				"DELETE FROM `%PREFIX%active` WHERE `id` = ?", 
-				"DELETE FROM %PREFIX%active WHERE id = ?"),
+				"DELETE FROM `%PREFIX%active` WHERE `id` = ?"),
 		
 		REFRESH_ACTIVE(
-				"DELETE FROM `%PREFIX%active` WHERE `expiration` <= ? AND `expiration` != -1",
-				"DELETE FROM %PREFIX%active WHERE expiration <= ? AND expiration != -1"),
+				"DELETE FROM `%PREFIX%active` WHERE `expiration` <= ? AND `expiration` != -1"),
 		
 		UPDATE_ACTIVE_REASON_FOR_ID(
-				"UPDATE `%PREFIX%active` SET `reason` = ? WHERE `id` = ?",
-				"UPDATE %PREFIX%active SET reason = ? WHERE id = ?"),
+				"UPDATE `%PREFIX%active` SET `reason` = ? WHERE `id` = ?"),
 		
 		UPDATE_HISTORY_REASON_FOR_ID(
-				"UPDATE `%PREFIX%history` SET `reason` = ? WHERE `id` = ?",
-				"UPDATE %PREFIX%history SET reason = ? WHERE id = ?"),
+				"UPDATE `%PREFIX%history` SET `reason` = ? WHERE `id` = ?"),
 		
 		UPDATE_CACHE_FOR_UUID(
-				"UPDATE `%PREFIX%cache` SET `name` = ?, `iplist` = ?, `update-name` = ?, `update-iplist` = ? WHERE `uuid` = ?",
-				"UPDATE %PREFIX%cache SET name = ?, iplist = ?, update-name = ?, update-iplist = ? WHERE uuid = ?"),
+				"UPDATE `%PREFIX%cache` SET `name` = ?, `iplist` = ?, `update-name` = ?, `update-iplist` = ? WHERE `uuid` = ?"),
 		
 		UPDATE_NAME_FOR_UUID(
-				"UPDATE `%PREFIX%cache` SET `name` = ?, `update-name` = ? WHERE `uuid` = ?",
-				"UPDATE %PREFIX%cache SET name = ?, update-name = ? WHERE uuid = ?"),
+				"UPDATE `%PREFIX%cache` SET `name` = ?, `update-name` = ? WHERE `uuid` = ?"),
 		
 		UPDATE_IPS_FOR_UUID(
-				"UPDATE `%PREFIX%cache` SET `iplist` = ?, `update-iplist` = ? WHERE `uuid` = ?",
-				"UPDATE %PREFIX%cache SET iplist = ?, update-iplist = ? WHERE uuid = ?"),
+				"UPDATE `%PREFIX%cache` SET `iplist` = ?, `update-iplist` = ? WHERE `uuid` = ?"),
 		
-		SELECT_ALL_ACTIVE(
-				"SELECT * FROM `%PREFIX%active`",
-				"SELECT * FROM %PREFIX%active"),
+		SELECT_ALL_ACTIVE("SELECT * FROM `%PREFIX%active`"),
 		
-		SELECT_ALL_HISTORY(
-				"SELECT * FROM `%PREFIX%history`",
-				"SELECT * FROM %PREFIX%history"),
+		SELECT_ALL_HISTORY("SELECT * FROM `%PREFIX%history`"),
 		
-		SELECT_ALL_CACHED(
-				"SELECT * FROM `%PREFIX%cache`",
-				"SELECT * FROM %PREFIX%cache");
+		SELECT_ALL_CACHED("SELECT * FROM `%PREFIX%cache`");
 
 		private String mysql;
 
 		private Query(String mysql) {
 			this.mysql = mysql;
-		}
-		
-		@Deprecated
-		private Query(String mysql, String file) {
-			this(mysql);
 		}
 		
 		String eval(SqlSettings settings) {
