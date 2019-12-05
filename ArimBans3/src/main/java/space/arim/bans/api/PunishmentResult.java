f83903cfd9fe3c79f5b2acec0b1855c523998616
A248 <anandebeh@gmail.com>
2019-12-05 17:37:13 -0500

    Add PunishmentResult wrapper

---
+++ ArimBans3/src/main/java/space/arim/bans/api/PunishmentResult.java
@@ -0,0 +1,58 @@
+/*
+ * ArimBans, a punishment plugin for minecraft servers
+ * Copyright © 2019 Anand Beh <https://www.arim.space>
+ * 
+ * ArimBans is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimBans is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimBans. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.bans.api;
+
+public class PunishmentResult {
+	
+	private final boolean has;
+	private final Subject subject;
+	private final Punishment punishment;
+	private final String message;
+	
+	public PunishmentResult(Subject subject, Punishment punishment, String message) {
+		has = true;
+		this.subject = subject;
+		this.punishment = punishment;
+		this.message = message;
+	}
+	
+	public PunishmentResult() {
+		has = false;
+		subject = null;
+		punishment = null;
+		message = null;
+	}
+	
+	public boolean hasPunishment() {
+		return has;
+	}
+	
+	public Subject getPossibleApplicableSubject() {
+		return subject;
+	}
+	
+	public Punishment getPossiblePunishment() {
+		return punishment;
+	}
+	
+	public String getPossibleMessage() {
+		return message;
+	}
+	
+}
