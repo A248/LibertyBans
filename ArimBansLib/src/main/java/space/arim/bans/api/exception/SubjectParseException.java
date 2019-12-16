eed9d55d892bb27e89d7605a1cec16bf067d7a45
A248 <anandebeh@gmail.com>
2019-12-16 07:08:45 -0500

    Update license headers for ArimBansLib

--- ArimBansLib/src/main/java/space/arim/bans/api/exception/SubjectParseException.java
+++ ArimBansLib/src/main/java/space/arim/bans/api/exception/SubjectParseException.java
@@ -1,19 +1,19 @@
-/*
- * ArimBans, a punishment plugin for minecraft servers
+/* 
+ * ArimBansLib, an API for ArimBans
  * Copyright © 2019 Anand Beh <https://www.arim.space>
  * 
- * ArimBans is free software: you can redistribute it and/or modify
+ * ArimBansLib is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * ArimBans is distributed in the hope that it will be useful,
+ * ArimBansLib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with ArimBans. If not, see <https://www.gnu.org/licenses/>
+ * along with ArimBansLib. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
 package space.arim.bans.api.exception;
