99a378a4b182d5e1a10a0e21281a28ea3cfedd7f
A248 <anandebeh@gmail.com>
2019-12-18 15:40:15 -0500

    Start work on ArimBansSkriptPlugin

---
+++ ArimBansSkript/src/main/java/space/arim/bans/skript/ArimBansSkriptPlugin.java
@@ -0,0 +1,35 @@
+/* 
+ * ArimBansSkript, a skript addon for ArimBans
+ * Copyright © 2019 Anand Beh <https://www.arim.space>
+ * 
+ * ArimBansSkript is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimBansSkript is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimBansSkript. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.bans.skript;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArimBansSkriptPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
