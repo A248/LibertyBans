28571fa3fe547482fa010a914ac81f1a6c7523e2
A248 <anandebeh@gmail.com>
2019-12-31 09:42:21 -0500

    Improve Commands#listCmd, remove usage of package-private Lister class

--- ArimBans3/src/main/java/space/arim/bans/internal/frontend/commands/Commands.java
+++ ArimBans3/src/main/java/space/arim/bans/internal/frontend/commands/Commands.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 
 import space.arim.bans.ArimBans;
@@ -46,6 +47,7 @@ import space.arim.bans.api.exception.NoGeoIpException;
 import space.arim.bans.api.exception.TypeParseException;
 
 import space.arim.universal.util.UniversalUtil;
+import space.arim.universal.util.collections.CollectionsUtil;
 
 import space.arim.api.util.StringsUtil;
 import space.arim.api.util.web.GeoIpInfo;
@@ -578,32 +580,50 @@ public class Commands implements CommandsMaster {
 		return getCmdBaseString(command) + " " + nextPage;
 	}
 	
+	private Predicate<Punishment> getListFilter(CommandType command, Subject target) {
+		switch (command.subCategory()) {
+		case BANLIST:
+			return (punishment) -> !punishment.type().equals(PunishmentType.BAN) || command.ipSpec().equals(IpSpec.UUID) && !punishment.subject().getType().equals(Subject.SubjectType.PLAYER) || command.ipSpec().equals(IpSpec.IP) && !punishment.subject().getType().equals(Subject.SubjectType.IP);
+		case MUTELIST:
+			return (punishment) -> !punishment.type().equals(PunishmentType.MUTE) || command.ipSpec().equals(IpSpec.UUID) && !punishment.subject().getType().equals(Subject.SubjectType.PLAYER) || command.ipSpec().equals(IpSpec.IP) && !punishment.subject().getType().equals(Subject.SubjectType.IP);
+		case WARNS:
+			return (punishment) -> !punishment.subject().equals(target) || !punishment.type().equals(PunishmentType.WARN);
+		case HISTORY:
+			return (punishment) -> !punishment.subject().equals(target);
+		case BLAME:
+			return (punishment) -> !punishment.operator().equals(target);
+		default:
+			return (punishment) -> false;
+		}
+	}
+	
+	private static String replaceListVars(String input, int page, int maxPage, String nextPageCmd) {
+		return input.replace("%PAGE%", Integer.toString(page)).replace("%MAXPAGE%", Integer.toString(maxPage)).replace("%CMD%", nextPageCmd);
+	}
+	
 	private void listCmd(Subject operator, Subject target, CommandType command, int page) {
-		Set<Punishment> applicable = SubCategory.HISTORY.equals(command.subCategory()) ? center.punishments().getHistoryCopy() : center.punishments().getActiveCopy();
-		list(operator, new Lister<Punishment>(page, perPage.get(command.subCategory()), maxPage.get(command.subCategory()), noPage.get(command.subCategory()), nextPageCmd(command, page), applicable, header.get(command.subCategory()), body.get(command.subCategory()), footer.get(command.subCategory())) {
-			@Override
-			public boolean check(Punishment punishment) {
-				if (SubCategory.BANLIST.equals(command.subCategory()) || SubCategory.MUTELIST.equals(command.subCategory())) {
-					if (command.ipSpec().equals(IpSpec.UUID) && !punishment.subject().getType().equals(Subject.SubjectType.PLAYER) || command.ipSpec().equals(IpSpec.IP) && !punishment.subject().getType().equals(Subject.SubjectType.IP)) {
-						return false;
-					}
-				}
-				switch (command.subCategory()) {
-				case BANLIST:
-					return PunishmentType.BAN.equals(punishment.type());
-				case MUTELIST:
-					return PunishmentType.MUTE.equals(punishment.type());
-				case WARNS:
-					return punishment.subject().equals(target) && PunishmentType.WARN.equals(punishment.type());
-				case HISTORY:
-					return punishment.subject().equals(target);
-				case BLAME:
-					return punishment.operator().equals(target);
-				default:
-					return true;
-				}
-			}
+		List<Punishment> punishments = new ArrayList<Punishment>(SubCategory.HISTORY.equals(command.subCategory()) ? center.punishments().getHistoryCopy() : center.punishments().getActiveCopy());
+		punishments.removeIf(getListFilter(command, target));
+		if (punishments.isEmpty()) {
+			center.subjects().sendMessage(operator, noPage.get(command.subCategory()));
+			return;
+		}
+		String maxPagesMsg = maxPage.get(command.subCategory());
+		int maxPage = (punishments.size() - 1)/perPage.get(command.subCategory()) + 1;
+		if (page > maxPage) {
+			center.subjects().sendMessage(operator, maxPagesMsg.replace("%PAGE%", Integer.toString(page)).replace("%MAXPAGE%", Integer.toString(maxPage)));
+			return;
+		}
+		List<String> headerList = header.get(command.subCategory());
+		List<String> bodyList = body.get(command.subCategory());
+		List<String> footerList = footer.get(command.subCategory());
+		String nextPageCmd = nextPageCmd(command, page);
+		punishments.sort(DATE_LATEST_FIRST_COMPARATOR);
+		center.subjects().sendMessage(operator, false, CollectionsUtil.wrapAll(headerList.toArray(new String[] {}), (headerItem) -> replaceListVars(headerItem, page, maxPage, nextPageCmd)));
+		punishments.forEach((punishment) -> {
+			center.subjects().sendMessage(operator, true, CollectionsUtil.wrapAll(bodyList.toArray(new String[] {}), (bodyItem) -> replaceListVars(center.formats().formatMessageWithPunishment(bodyItem, punishment), page, maxPage, nextPageCmd)));
 		});
+		center.subjects().sendMessage(operator, true, CollectionsUtil.wrapAll(footerList.toArray(new String[] {}), (footerItem) -> replaceListVars(footerItem, page, maxPage, nextPageCmd)));
 	}
 	
 	private void otherCmd(Subject operator, Subject target, CommandType command, String[] extraArgs) {
@@ -643,6 +663,7 @@ public class Commands implements CommandsMaster {
 				warns++;
 			}
 		}
+		final int warnCount = warns;
 		String header;
 		String info;
 		if (target.getType().equals(SubjectType.PLAYER)) {
@@ -656,12 +677,10 @@ public class Commands implements CommandsMaster {
 		}
 		String banStatus = banPunishment != null ? center.formats().formatMessageWithPunishment(other_status_layout.get("ban-status.banned"), banPunishment) : other_status_layout.get("ban-status.not-banned");
 		String muteStatus = mutePunishment != null ? center.formats().formatMessageWithPunishment(other_status_layout.get("mute-status.muted"), mutePunishment) : other_status_layout.get("mute-status.not-muted");
-		String warnStatus = warns != 0 ? other_status_layout.get("warn-status.warn-count") : other_status_layout.get("warn-status.no-warns");
+		String warnStatus = warnCount != 0 ? other_status_layout.get("warn-status.warn-count") : other_status_layout.get("warn-status.no-warns");
+		String targetDisplay = center.formats().formatSubject(target);
 		String[] msgs = other_status_layout_body.toArray(new String[0]);
-		for (int n = 0; n < msgs.length; n++) {
-			msgs[n] = msgs[n].replace("%HEADER%", header).replace("%INFO%", info).replace("%BAN_STATUS%", banStatus).replace("%MUTE_STATUS%", muteStatus).replace("%WARN_STATUS%", warnStatus);
-		}
-		center.subjects().sendMessage(operator, msgs);
+		center.subjects().sendMessage(operator, CollectionsUtil.wrapAll(msgs, (msg) -> msg.replace("%TARGET%", targetDisplay).replace("%HEADER%", header).replace("%INFO%", info).replace("%BAN_STATUS%", banStatus).replace("%MUTE_STATUS%", muteStatus).replace("%WARN_STATUS%", warnStatus).replace("%WARN_COUNT%", Integer.toString(warnCount)).replace("%CMD_WARNS%", "arimbans warns " + targetDisplay)));
 	}
 	
 	private void ipsCmd(Subject operator, Subject target) {
@@ -773,39 +792,6 @@ public class Commands implements CommandsMaster {
 			}
 		}
 		center.subjects().sendMessage(operator, other_rollback_successful_message.replace("%NUMBER%", numberArg).replace("%TARGET%", center.formats().formatSubject(target)));
-	}
-	
-	private void list(Subject operator, Lister<Punishment> lister) {
-		ArrayList<Punishment> punishments = new ArrayList<Punishment>(lister.applicable);
-		punishments.removeIf((punishment) -> !lister.check(punishment));
-		if (punishments.isEmpty()) {
-			center.subjects().sendMessage(operator, lister.noPagesMsg);
-			return;
-		}
-		int maxPage = (punishments.size() - 1)/lister.perPage + 1;
-		if (lister.page > maxPage) {
-			center.subjects().sendMessage(operator, lister.maxPagesMsg.replace("%PAGE%", Integer.toString(lister.page)).replace("%MAXPAGE%", Integer.toString(maxPage)));
-			return;
-		}
-		List<String> header = lister.header;
-		List<String> body = lister.body;
-		List<String> footer = lister.footer;
-		for (int n = 0; n < header.size(); n++) {
-			header.set(n, header.get(n).replace("%PAGE%", Integer.toString(lister.page)).replace("%MAXPAGE%", Integer.toString(maxPage)).replace("%CMD%", lister.nextPageCmd));
-		}
-		for (int n = 0; n < footer.size(); n++) {
-			footer.set(n, footer.get(n).replace("%PAGE%", Integer.toString(lister.page)).replace("%MAXPAGE%", Integer.toString(maxPage)).replace("%CMD%", lister.nextPageCmd));
-		}
-		punishments.sort(DATE_LATEST_FIRST_COMPARATOR);
-		center.subjects().sendMessage(operator, header.toArray(new String[0]));
-		for (Punishment p : punishments) {
-			String[] msgs = body.toArray(new String[0]);
-			for (int n = 0; n < msgs.length; n++) {
-				msgs[n] = center.formats().formatMessageWithPunishment(msgs[n], p).replace("%PAGE%", Integer.toString(lister.page)).replace("%MAXPAGE%", Integer.toString(maxPage)).replace("%CMD%", lister.nextPageCmd);
-			}
-			center.subjects().sendMessage(operator, true, msgs);
-		}
-		center.subjects().sendMessage(operator, true, footer.toArray(new String[0]));
 	}
 
 	@Override
@@ -971,33 +957,5 @@ public class Commands implements CommandsMaster {
 	public void noPermission(Subject subject, CommandType command) {
 		center.subjects().sendMessage(subject, perm.get(command));
 	}
-	
-}
-
-abstract class Lister<T> {
-	
-	final int page;
-	final int perPage;
-	final String maxPagesMsg;
-	final String noPagesMsg;
-	final String nextPageCmd;
-	final Set<T> applicable;
-	final List<String> header;
-	final List<String> body;
-	final List<String> footer;
-	
-	Lister(int page, int perPage, String maxPagesMsg, String noPagesMsg, String nextPageCmd, Set<T> applicable, List<String> header, List<String> body, List<String> footer) {
-		this.page = page;
-		this.perPage = perPage;
-		this.maxPagesMsg = maxPagesMsg;
-		this.noPagesMsg = noPagesMsg;
-		this.nextPageCmd = nextPageCmd;
-		this.applicable = applicable;
-		this.header = header;
-		this.body = body;
-		this.footer = footer;
-	}
-	
-	abstract boolean check(T object);
 	
 }
