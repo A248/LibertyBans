4f9508022425479e47fc20e4d8396e0713fc09cd
A248 <anandebeh@gmail.com>
2020-07-05 12:13:53 -0400

    Add PunishmentTypeTest

---
+++ bans-api/src/test/java/space/arim/libertybans/api/PunishmentTypeTest.java
@@ -0,0 +1,34 @@
+/* 
+ * LibertyBans-api
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * LibertyBans-api is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * LibertyBans-api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LibertyBans-api. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Affero General Public License.
+ */
+package space.arim.libertybans.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PunishmentTypeTest {
+
+	@Test
+	public void testOrdinals() {
+		for (PunishmentType type : PunishmentType.values()) {
+			assertEquals(type, PunishmentType.fromOrdinal(type.ordinal()));
+		}
+	}
+	
+}
