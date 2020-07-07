7108317b25e5826752d723cf88e4c3f937754cbc
A248 <anandebeh@gmail.com>
2020-07-07 14:39:03 -0400

    Define separate major and minor version applying to SQL backend

--- bans-api/src/main/java/space/arim/libertybans/api/PunishmentDatabase.java
+++ bans-api/src/main/java/space/arim/libertybans/api/PunishmentDatabase.java
@@ -24,11 +24,26 @@ import java.util.concurrent.Executor;
 
 /**
  * Access to the raw database. <b>Use of this should be avoided at great cost. Executing SQL queries
- * is not considered API. </b> <br>
+ * is not considered traditional API.</b> <br>
  * <br>
  * The majority of users should never touch this directly. Instead, {@link PunishmentSelector} provides
  * predicate, error-free specifications using stable API. {@code PunishmentSelector} may be used to retrieve
- * punishments with all sorts of details from the database.
+ * punishments with all sorts of details from the database. <br>
+ * <br>
+ * <a>API Status</b> <br>
+ * Use of the SQL backend directly is not considered traditional API. Although attempts will be made to limit
+ * the number of breaking changes, LibertyBans's major version will not necessarily change to match the SQL backend.
+ * Unlike other plugins, LibertyBans makes use of a wide swath of queries. <br>
+ * <br>
+ * Rather, a separate version is maintained specifically for the SQL backend. This version is maintained returned by
+ * {@link #getMajorRevision()} and {@link #getMinorRevision()}. Additionally, it is located in the table
+ * <code>libertybans_revision</code>, with the {@code major} and {@code minor} columns indicating the major version
+ * and minor version, respectively (both columns use the INT data type). <br>
+ * <br>
+ * Whenever the tables and/or views definitions change incompatibly, the major version will be incremented. The minor
+ * version will be incremented as new tables and views are added, but which do not break backwards compatibility.
+ * <b>Use of any SQL functions or procedures created by LibertyBans is strictly unsupported and their presence might
+ * as well be considered coincidental</b>
  * 
  * @author A248
  *
@@ -36,17 +51,28 @@ import java.util.concurrent.Executor;
 public interface PunishmentDatabase {
 
 	/**
-	 * Gets a connection from the LibertyBans connection pool. <b>Use of this should be avoided at great cost.
-	 * Executing SQL queries is not considered API. </b> <br>
-	 * <br>
-	 * The only API guarantee with respect to executing API queries is that the table {@code libertybans_revision}
-	 * will contain a single row whose {@code revision} column indicates the data format LibertyBans maintains.
-	 * It is guaranteed that whenever the table definitions change, this {@code revision} will be incremented.
+	 * Gets a connection from the LibertyBans connection pool. <br>
+	 * <b>Use of this should be avoided at great cost. Executing SQL queries is not considered traditional API.</b>
+	 * See the class javadoc for more information.
 	 * 
-	 * @return a connection from the plugin pool
+	 * @return a connection from the plugin pool, which should be closed after immediately use
 	 * @throws SQLException as relayed from JDBC
 	 */
 	Connection getConnection() throws SQLException;
+	
+	/**
+	 * Gets the major revision of the SQL backend definitions, see class javadoc
+	 * 
+	 * @return the major revision
+	 */
+	int getMajorRevision();
+	
+	/**
+	 * Gets the minor revision of the SQL backend definitions, see class javadoc
+	 * 
+	 * @return the minor revision
+	 */
+	int getMinorRevision();
 	
 	/**
 	 * Gets the executor used by LibertyBans to execute queries. Typically, this executor
