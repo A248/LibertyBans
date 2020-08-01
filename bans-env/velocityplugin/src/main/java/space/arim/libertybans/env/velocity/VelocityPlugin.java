a82dae2c953e3413d339759c5b661b12a799e2ce
A248 <anandebeh@gmail.com>
2020-08-01 19:01:25 -0400

    Initial work on Velocity and Sponge support

---
+++ bans-env/velocityplugin/src/main/java/space/arim/libertybans/env/velocity/VelocityPlugin.java
@@ -0,0 +1,29 @@
+/* 
+ * LibertyBans-env-velocityplugin
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * LibertyBans-env-velocityplugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * LibertyBans-env-velocityplugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LibertyBans-env-velocityplugin. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Affero General Public License.
+ */
+package space.arim.libertybans.env.velocity;
+
+import space.arim.libertybans.bootstrap.plugin.PluginInfo;
+
+import com.velocitypowered.api.plugin.Plugin;
+
+@Plugin(id = PluginInfo.ANNOTE_ID, name = PluginInfo.NAME, version = PluginInfo.VERSION, authors = {
+		"A248" }, description = PluginInfo.DESCRIPTION, url = PluginInfo.URL)
+public class VelocityPlugin {
+
+}
