6d93dbd7b92b490d28d6a57fb124b68c6764d236
A248 <theanandbeh@gmail.com>
2022-08-04 10:01:41 -0400

    Turn this test into a passing one

--- bans-core/src/main/resources/database-migrations/V20__Stricter_data_integrity_checks.sql
+++ bans-core/src/main/resources/database-migrations/V20__Stricter_data_integrity_checks.sql
@@ -1,7 +1,7 @@
 
--- ALTER TABLE "${tableprefix}addresses"
--- ADD CONSTRAINT "${tableprefix}address_length"
--- CHECK (OCTET_LENGTH("address") IN (4, 16));
+ALTER TABLE "${tableprefix}addresses"
+  ADD CONSTRAINT "${tableprefix}address_length"
+  CHECK (OCTET_LENGTH("address") IN (4, 16));
 
 ALTER TABLE "${tableprefix}victims"
   ADD CONSTRAINT "${tableprefix}victim_address_length"
