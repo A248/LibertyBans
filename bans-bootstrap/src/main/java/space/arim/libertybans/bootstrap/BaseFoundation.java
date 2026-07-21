ccb602b8fe07c498a60052a6358ba8f0fe3a6e09
A248 <a248-dev@tuta.io>
2026-07-21 19:09:07 -0400

    Relicense components where relevant to be compatible with Fabric
    * Prevent any AGPL code from linking directly to NMS or Fabric
      APIs. This means that LibertyBans stays independent of the NMS
      server and vice-versa at all times; one can be used without the
      other, and AGPL code is never written against proprietary APIs.
    * In this model, the platform implementation and bootstrap code
      interact with both the core plugin and the proprietary server.
      However, neither is aware of the other.

--- bans-bootstrap/src/main/java/space/arim/libertybans/bootstrap/BaseFoundation.java
+++ bans-bootstrap/src/main/java/space/arim/libertybans/bootstrap/BaseFoundation.java
@@ -1,20 +1,20 @@
 /*
- * LibertyBans
- * Copyright © 2025 Anand Beh
+ * LibertyBans-bootstrap
+ * Copyright © 2026 Anand Beh
  *
- * LibertyBans is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * LibertyBans-bootstrap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * LibertyBans is distributed in the hope that it will be useful,
+ * LibertyBans-bootstrap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with LibertyBans. If not, see <https://www.gnu.org/licenses/>
- * and navigate to version 3 of the GNU Affero General Public License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LibertyBans-bootstrap. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
  */
 
 package space.arim.libertybans.bootstrap;
@@ -36,7 +36,8 @@ public interface BaseFoundation {
 	}
 
 	/**
-	 * Used for Sponge and standalone only. Sponge requires early command registration and service provision
+	 * Used for some platforms only, depending on their specific needs. For example, Fabric and Sponge have a
+	 * command registration event that happens before main plugin startup.
 	 *
 	 * @return the platform accessors
 	 */
