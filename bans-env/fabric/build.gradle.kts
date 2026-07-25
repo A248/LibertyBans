0a351eb5c037a7b8a6e3ce7481aebe3f39e8ea10
A248 <a248-dev@tuta.io>
2026-07-25 03:10:04 -0400

    Attempt to limit metadata and see if works

--- bans-env/fabric/build.gradle.kts
+++ bans-env/fabric/build.gradle.kts
@@ -57,7 +57,27 @@ allprojects {
 
 subprojects {
     repositories {
-        mavenCentral()
+        maven {
+            name = "local-build-artifacts"
+            url = uri("${project.rootDir}/target/maven/build-artifact-repo")
+            metadataSources {
+                mavenPom()
+            }
+        }
+        maven {
+            name = "local-maven-repo"
+            url = uri("${project.rootDir}/../../.mvn/local-repo")
+            metadataSources {
+                mavenPom()
+            }
+        }
+        maven {
+            name = "central"
+            url = uri("https://repo.maven.org/maven2")
+            metadataSources {
+                mavenPom()
+            }
+        }
         maven {
             name = "fabricmc"
             url = uri("https://maven.fabricmc.net/")
@@ -66,26 +86,16 @@ subprojects {
             name = "arim-mvn-lgpl3"
             url = uri("https://mvn-repo.arim.space/lesser-gpl3/")
             //includeGroupAndSubgroups("space.arim")
+            metadataSources {
+                mavenPom()
+            }
         }
         maven {
             name = "arim-mvn-gpl3"
             url = uri("https://mvn-repo.arim.space/gpl3/")
             //includeGroupAndSubgroups("space.arim")
-        }
-        maven {
-            name = "local-build-artifacts"
-            url = uri("${project.rootDir}/target/maven/build-artifact-repo")
             metadataSources {
                 mavenPom()
-                artifact()
-            }
-        }
-        maven {
-            name = "local-maven-repo"
-            url = uri("${project.rootDir}/../../.mvn/local-repo")
-            metadataSources {
-                mavenPom()
-                artifact()
             }
         }
     }
